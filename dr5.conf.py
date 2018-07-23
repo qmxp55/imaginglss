7668bc76cb645c52c6d6f5ace1cd98403f3bf8d1
qmxp55 <omar.a.ruiz-macias@durham.ac.uk>
2018-07-23 09:32:28 -0700

    conf file directory change

--- dr5.conf.py
+++ dr5.conf.py
@@ -1,10 +1,10 @@
 decals_root = '/global/project/projectdirs/cosmo/data/legacysurvey/dr5'
 decals_cache = '/global/project/projectdirs/desi/users/qmxp55/cache'
-imaginglss_path = '/global/project/projectdirs/desi/users/qmxp55/imaginglss_test'
+imaginglss_path = '/global/project/projectdirs/desi/users/qmxp55/imaginglss'
 
 decals_release = "DR5"
 sweep_dir = os.path.abspath(os.path.join(decals_root, 'sweep/5.0'))
 tycho_dir = os.path.abspath(os.path.join(imaginglss_path, 'tycho2.fits'))
 wise_dir = os.path.abspath(os.path.join(imaginglss_path, 'WISE-BRIGHT.hdf5'))
 dust_dir = "/project/projectdirs/desi/software/edison/dust/v0_0/"
-twomass_dir = "/global/project/projectdirs/desi/users/qmxp55/imaginglss_test/asu_j_ext_12.txt"
+twomass_dir = "/global/project/projectdirs/desi/users/qmxp55/imaginglss/asu_j_ext_12.txt"
